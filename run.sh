4813e82eb6f0dec83ebc1ba4e959bfc26e624a91
Christy <liyuanchristy@gmail.com>
2017-06-06 15:11:43 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python train_predict_cv.py --data_dir './data' --train_filename 'train_tiny.csv' --test_filename 'test_tiny.csv'
+python train_predict_cv.py --data_dir './data' --train_filename 'train.csv' --test_filename 'test_tiny.csv'
