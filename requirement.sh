9ac8ac000e7c151d24a26e9380bc2a0687d1369b
Christy <liyuanchristy@gmail.com>
2017-06-06 15:01:45 -0400

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -1,5 +1,6 @@
-pip install pandas 
+pip install pandas
 pip install sklearn
+
 git clone --recursive https://github.com/Microsoft/LightGBM 
 cd LightGBM
 mkdir build ; cd build
@@ -7,3 +8,12 @@ cmake ..
 make -j 
 cd ../python-package; python setup.py install
 cd ../../
+
+cd data
+git clone https://github.com/petuum-inc/ml-storage.git
+mv ml-storage/*.zip ./
+unzip train_v2.csv.zip
+unzip test_v2.csv.zip
+rm train_v2.csv.zip
+rm test_v2.csv.zip
+cd ../
